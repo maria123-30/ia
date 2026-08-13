56ae09209a4a73daf1d6de117633289c4007d0bf
maria123-30 <m.rosa30@escola.pr.gov.br>
2026-08-13 13:21:40 -0300

    j

--- script.js
+++ script.js
@@ -9,16 +9,17 @@ const perguntas = [
     enunciado:
       "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
     alternativas:[
-     {texto;"Isso é assustador!", 
+     {
+      texto;"Isso é assustador!", 
       afirmação;"afirmação"
-     }
+     },
      {
       texto;"Isso é maravilhoso!"
        afirmação;"afirmação"
      }
     
     ]
-  }
+  },
 
   {
     enunciado:
